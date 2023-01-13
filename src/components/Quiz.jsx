31bca641fd391b45ba599b59a087d7e74757da79
Tim <chibuiketimokonkwo@gmail.com>
2023-01-13 15:23:02 +0100

    updates

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,29 +1,33 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Question from "./Question";
-import Loader from "../../utils/Loader"
+import Loader from "../../utils/Loader";
 // import {nanoid} from "nanoid"
 
 export default function Quiz(props) {
 	const [loading, setLoading] = useState(true);
 
-	const questions = props.quiz.map((question) => 
-		<Question
-			id={question.question}
-			key={question.question}
-			question={question.question}
-			correct_answer={question.correct_answer}
-			incorrect_answers={question.incorrect_answers}
-		/>
-	);
+	try {
+		const questions = props.quiz.map((question) => (
+			<Question
+				id={question.question}
+				key={question.question}
+				question={question.question}
+				correct_answer={question.correct_answer}
+				incorrect_answers={question.incorrect_answers}
+			/>
+		));
 
-	// console.log(questions)
-
-	return (
-		<div className="quiz grid">
-			{questions}
-			{/* <Loader /> */}
-			<button>Check answers</button>
-		</div>
-	);
+		return (
+			<div className="quiz grid">
+				{questions}
+				<button>Check answers</button>
+			</div>
+		);
+	} catch (err) {
+		console.log("Network issue");
+		return (
+			<Loader />
+		)
+	}
 }
