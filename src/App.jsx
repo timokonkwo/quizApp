84c6d8d465571c8d7f921207f09e4cb755ec9d56
Tim <chibuiketimokonkwo@gmail.com>
2023-01-19 21:40:39 +0100

    added local data for test purposes

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,19 @@ import Question from "./components/Question";
 import Loader from "../utils/Loader";
 import Quiz from "./components/Quiz";
 import { useEffect } from "react";
+import { data } from "./data";
 
 export default function App() {
 	const [page, setPage] = useState("intro");
 	const [quiz, setQuiz] = useState(null);
 	const [loading, setLoading] = useState(true);
 
-
 	useEffect(() => {
-		fetch("https://opentdb.com/api.php?amount=10&type=multiple")
-			.then((response) => response.json())
-			.then((data) => setQuiz(data.results))
-			.catch((err) => console.log("error"));
+		// fetch("https://opentdb.com/api.php?amount=10&type=multiple")
+		// 	.then((response) => response.json())
+		// 	.then((data) => setQuiz(data.results))
+		// 	.catch((err) => console.log("error"));
+		setQuiz(data);
 	}, [loading]);
 
 	function startQuiz() {
---
+++ src/data.js
@@ -0,0 +1,106 @@
+export const data = [
+    {
+        category: "Entertainment: Film",
+        type: "multiple",
+        difficulty: "medium",
+        question:
+            "This movie contains the quote, &quot;What we&#039;ve got here is a failure to communicate.&quot;",
+        correct_answer: "Cool Hand Luke",
+        incorrect_answers: [
+            "Bonnie and Clyde",
+            "The Graduate",
+            "In the Heat of the Night",
+        ],
+    },
+    {
+        category: "Entertainment: Film",
+        type: "multiple",
+        difficulty: "medium",
+        question:
+            "What is the name of the first &quot;Star Wars&quot; film by release order?",
+        correct_answer: "A New Hope",
+        incorrect_answers: [
+            "The Phantom Menace",
+            "The Force Awakens",
+            "Revenge of the Sith",
+        ],
+    },
+    {
+        category: "General Knowledge",
+        type: "multiple",
+        difficulty: "medium",
+        question:
+            "What alcoholic drink is mainly made from juniper berries?",
+        correct_answer: "Gin",
+        incorrect_answers: ["Vodka", "Rum", "Tequila"],
+    },
+    {
+        category: "Entertainment: Books",
+        type: "multiple",
+        difficulty: "hard",
+        question:
+            "The novel &quot;Jane Eyre&quot; was written by what author? ",
+        correct_answer: "Charlotte Bront&euml;",
+        incorrect_answers: [
+            "Emily Bront&euml;",
+            "Jane Austen",
+            "Louisa May Alcott",
+        ],
+    },
+    {
+        category: "Science & Nature",
+        type: "multiple",
+        difficulty: "medium",
+        question: "About how old is Earth?",
+        correct_answer: "4.5 Billion Years",
+        incorrect_answers: [
+            "3.5 Billion Years",
+            "2.5 Billion Years",
+            "5.5 Billion Years",
+        ],
+    },
+    {
+        category: "Science & Nature",
+        type: "multiple",
+        difficulty: "hard",
+        question: "Coulrophobia is the irrational fear of what?",
+        correct_answer: "Clowns",
+        incorrect_answers: ["Cemeteries", "Needles", "Tunnels"],
+    },
+    {
+        category: "Entertainment: Music",
+        type: "multiple",
+        difficulty: "easy",
+        question:
+            "&quot;Doctor Jones&quot;, &quot;Turn Back Time&quot; and &quot;Barbie Girl&quot; were UK number ones for which Eurodance group?",
+        correct_answer: "Aqua",
+        incorrect_answers: ["Vengaboys", "Eiffel 65", "Sash!"],
+    },
+    {
+        category: "Entertainment: Musicals & Theatres",
+        type: "multiple",
+        difficulty: "hard",
+        question:
+            "Which Shakespeare play features the stage direction &quot;Enter a messenger, with two heads and a hand&quot;?",
+        correct_answer: "Titus Andronicus",
+        incorrect_answers: ["Othello", "Macbeth", "King Lear"],
+    },
+    {
+        category: "General Knowledge",
+        type: "multiple",
+        difficulty: "hard",
+        question:
+            "In &quot;Battle Cats&quot;, what is Moneko / MISS Moneko&#039;s critical percentage rate?",
+        correct_answer: "15%",
+        incorrect_answers: ["20%", "10%", "25%"],
+    },
+    {
+        category: "General Knowledge",
+        type: "multiple",
+        difficulty: "medium",
+        question:
+            "What fast food chain has the most locations globally? ",
+        correct_answer: "Subway",
+        incorrect_answers: ["Starbucks", "McDonalds", "KFC"],
+    },
+]
