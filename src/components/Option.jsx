b028152aebccf4187fb78a52458eff56e8944bf5
Tim <chibuiketimokonkwo@gmail.com>
2023-01-25 20:25:11 +0100

    updates

--- src/components/Option.jsx
+++ src/components/Option.jsx
@@ -1,18 +1,27 @@
 // The single option component
 export default function Option(props) {
+
+    // Initialize the css class to be styled as an option
 	let cssClass = "option";
 
+    // Initialize variable to check the correct click event target
+    let targetOptionElement
+
+    // Checking of correct click event target
+    props.selected && props.selected == props.answer ? targetOptionElement = true : targetOptionElement = false
+
+    // If the quiz has ended, check for correct
     if (props.endQuiz){
-        if (props.selected && props.selected == props.answer && props.selected === props.correctAnswer && props.marked === "correct") {
+        if (targetOptionElement && props.selected === props.correctAnswer && props.marked === "correct") {
             cssClass = "option correct"
 
-        } else if (props.selected && props.selected == props.answer && props.marked == "wrong") {
+        } else if (targetOptionElement && props.marked == "wrong") {
             cssClass = "option wrong"
         }
 
     }
 
-    else if (props.selected && props.selected == props.answer){
+    else if (targetOptionElement){
         cssClass = "option selected"
     }
 
